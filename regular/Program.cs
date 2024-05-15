67dfbbd751952a2a180f12c079d7630f9891b3e0
12 <12@DESKTOP-4I7NVEA>
2024-05-15 08:38:31 +0300

    Добавьте файлы проекта.

---
+++ regular/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите строку:");
+string inputstring = Console.ReadLine();
+
+string needstring = @"\b(\w+)\b(.*?)\b\1\b";
+
+string resultstring = Regex.Replace(inputstring, needstring, @"<strong>$1</strong>$2<strong>$1</strong>");
+
+Console.WriteLine(resultstring);
